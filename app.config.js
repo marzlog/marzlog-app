3bbbb0f0cf16041f48a459386ffc7164223bebab
JS SUH <jssuh@jjsim.com>
2026-04-17 20:27:20 +0900

    fix: iOS GoogleService-Info.plist EAS Secret 분기 추가

--- app.config.js
+++ app.config.js
@@ -1,9 +1,14 @@
 const config = require("./app.json");
 
 const androidConfig = config.expo.android;
+const iosConfig = config.expo.ios;
 
 if (process.env.GOOGLE_SERVICES_JSON) {
   androidConfig.googleServicesFile = process.env.GOOGLE_SERVICES_JSON;
+}
+
+if (process.env.GOOGLE_SERVICES_INFOPLIST) {
+  iosConfig.googleServicesFile = process.env.GOOGLE_SERVICES_INFOPLIST;
 }
 
 module.exports = config;
